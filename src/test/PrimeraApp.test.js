79b47e2e04a5300f021df4945f9cf85ebbe7e451
mdCarron <mdcarron.dev@gmail.com>
2020-09-28 13:15:06 -0300

    Completa 14. Enzyme - Testing unit

--- src/test/PrimeraApp.test.js
+++ src/test/PrimeraApp.test.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
 import PrimeraApp from "../PrimeraApp";
+import { shallow } from "enzyme";
+import "@testing-library/jest-dom";
 
 describe("Testing en PrimeraApp", () => {
-  test("should mostrar mensaje Hola, soy un Titlo", () => {
+  /*   test("should mostrar mensaje Hola, soy un Titlo", () => {
     const saludo = "Hola, soy un Titulo";
     const { getByText } = render(<PrimeraApp saludo={saludo} />);
 
     expect(getByText(saludo)).toBeInTheDocument();
+  }); */
+  test("debe de mostrar <PrimeraApp /> correctamente", () => {
+    const saludo = "Hola, soy un Titulo";
+    const wrapper = shallow(<PrimeraApp saludo={saludo} />);
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
