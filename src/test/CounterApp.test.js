f14be23159ab833d3d83a43ef2c5efb51c86ab69
mdCarron <mdcarron.dev@gmail.com>
2020-09-28 22:03:27 -0300

    Completa 17. Simular eventos - Click

--- src/test/CounterApp.test.js
+++ src/test/CounterApp.test.js
@@ -4,9 +4,13 @@ import { shallow } from "enzyme";
 import "@testing-library/jest-dom";
 
 describe("Testing en CounterApp", () => {
-  test("should match snapshot", () => {
-    const wrapper = shallow(<CounterApp />);
+  let wrapper = shallow(<CounterApp />);
 
+  beforeEach(() => {
+    wrapper = shallow(<CounterApp />);
+  });
+
+  test("should match snapshot", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
@@ -17,5 +21,21 @@ describe("Testing en CounterApp", () => {
     const h2Value = wrapper.find("h2").text();
 
     expect(h2Value).toBe(`${nuevoValue}`);
+  });
+
+  test("should incrementar en 1", () => {
+    wrapper.find("button").at(1).simulate("click");
+    const counterText = wrapper.find("h2").text();
+
+    expect(counterText).toBe("11");
+  });
+
+  test("should decrementar en 1", () => {
+    const wrapper = shallow(<CounterApp />);
+
+    wrapper.find("button").at(0).simulate("click");
+    const counterText = wrapper.find("h2").text();
+
+    expect(counterText).toBe("9");
   });
 });
